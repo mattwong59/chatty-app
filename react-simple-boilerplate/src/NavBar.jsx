f7ef38e56bcf992390f92b0b386cda6a32cf5b40
mattwong59 <mattwong59@gmail.com>
2018-04-19 02:04:42 +0000

    Added some styling

--- react-simple-boilerplate/src/NavBar.jsx
+++ react-simple-boilerplate/src/NavBar.jsx
@@ -2,9 +2,13 @@ import React, {Component} from 'react';
 
 export default class NavBar extends Component {
   render(props) {
+
     return (
       <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty</a>
+        <a href="/" className="navbar-brand"><i className="far fa-comments"></i>Chatty</a>
+
+
+
         <p>{this.props.userCount} user(s) online</p>
       </nav>
     );
